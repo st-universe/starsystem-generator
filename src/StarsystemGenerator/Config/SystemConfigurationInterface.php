0fe8aaf4b033a3aee303e25bad2535dfa6b71453
Janko Richter <jrichter@intersoft.de>
2023-08-31 18:54:27 +0200

    interfaces

--- src/StarsystemGenerator/Config/SystemConfigurationInterface.php
+++ src/StarsystemGenerator/Config/SystemConfigurationInterface.php
@@ -8,19 +8,40 @@ interface SystemConfigurationInterface
 
     public function setMinSize(int $minSize): SystemConfigurationInterface;
 
+    public function getAllowedGrowthPercentage(): int;
+
     public function setAllowedGrowthPercentage(int $allowedGrowthPercentage): SystemConfigurationInterface;
+
+    public function hasPlanets(): bool;
 
     public function setHasPlanets(bool $hasPlanets): SystemConfigurationInterface;
 
+    public function hasMoons(): bool;
+
     public function setHasMoons(bool $hasMoons): SystemConfigurationInterface;
+
+    public function hasAsteroids(): bool;
 
     public function setHasAsteroids(bool $hasAsteroids): SystemConfigurationInterface;
 
+    /** @return array<int, int> */
+    public function getProbabilities(int $probabilityType): array;
+
     public function addPropability(int $type, int $percentage, int $probabilityType): SystemConfigurationInterface;
+
+    public function getMaxPlanets(): int;
 
     public function setMaxPlanets(int $maxPlanets): SystemConfigurationInterface;
 
+    public function getMaxMoons(): int;
+
     public function setMaxMoons(int $maxMoons): SystemConfigurationInterface;
 
+    public function getMaxAsteroids(): int;
+
     public function setMaxAsteroids(int $maxAsteroids): SystemConfigurationInterface;
+
+    public function getMassCenterDistanceHorizontal(): int;
+
+    public function getMassCenterDistanceVertical(): int;
 }
--- src/StarsystemGenerator/SystemMapDataInterface.php
+++ src/StarsystemGenerator/SystemMapDataInterface.php
@@ -4,9 +4,26 @@ namespace Stu\StarsystemGenerator;
 
 interface SystemMapDataInterface
 {
-    public function setFieldId(int $x, int $y, int $fieldId): SystemMapDataInterface;
+    public function getWidth(): int;
 
-    /** @return array<int, array<int, int>> */
+    public function getHeight(): int;
+
+    public function getFieldAmount(): int;
+
+    public function setFieldId(
+        int $x,
+        int $y,
+        int $fieldId,
+        int $fieldType
+    ): SystemMapDataInterface;
+
+    /** @return array<int, array{0: int, 1: int}> */
+    public function getAsteroidRing(int $radiusPercentage): array;
+
+    /** @return null|array<int, array{0: int, 1: int}> */
+    public function getPlanetDisplay(int $radiusPercentage, int $moonRange): ?array;
+
+    /** @return array<int, int> */
     public function getFieldData(): array;
 
     public function toString(bool $doPrint = false): string;
