9b429151f7767c235af46c47aad520e7bcae6beb
Janko Richter <jrichter@intersoft.de>
2023-08-31 19:41:30 +0200

    test

--- tests/StarsystemGenerator/SystemMapDataTest.php
+++ tests/StarsystemGenerator/SystemMapDataTest.php
@@ -2,19 +2,19 @@
 
 namespace Stu\StarsystemGenerator;
 
-use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Stu\StarsystemGenerator\Enum\FieldTypeEnum;
 
-final class SystemMapDataTest extends MockeryTestCase
+final class SystemMapDataTest extends StuTestCase
 {
     public function testToString(): void
     {
         $mapData = new SystemMapData(3, 2);
 
-        $mapData->setFieldId(2, 1, 5);
+        $mapData->setFieldId(2, 1, 5, FieldTypeEnum::ASTEROID);
 
         static::assertEquals(
             "0,5,0\n"
-                . "0,0,0",
+                . "0,0,0\n",
             $mapData->toString()
         );
     }
