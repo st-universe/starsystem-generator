dadb2818f029655d5423746785b783dd6cf03261
Janko Richter <jrichter@intersoft.de>
2023-08-31 14:41:39 +0200

    enum for field type

---
+++ src/StarsystemGenerator/Enum/FieldTypeEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\StarsystemGenerator\Enum;
+
+final class FieldTypeEnum
+{
+    public const MASS_CENTER = 1;
+    public const PLANET = 2;
+    public const MOON = 3;
+    public const ASTEROID = 4;
+}
